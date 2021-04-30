da28cf6a3e04be39cf906f93a76fd0f861ba872c
Angelica Quilme Manlapaz <77945056+Angelica2001143@users.noreply.github.com>
2021-04-30 19:57:17 +0800

    Edited number1

--- number1.php
+++ number1.php
@@ -2,8 +2,8 @@
 //number 1
 // Calculate the difference between two dates using PHP OOP approach.
 // Sample Dates : 1981-11-03, 2013-09-04
-$sdate = new DateTime("1981-11-03");
-$edate = new DateTime("2013-09-04");
-$interval = $sdate->diff($edate);
+$date1 = new DateTime("1981-11-03");
+$date2 = new DateTime("2013-09-04");
+$interval = $date1->diff($date2);
 echo "Difference : " . $interval->y . " years, " . $interval->m." months, ".$interval->d." days ";
 ?>
