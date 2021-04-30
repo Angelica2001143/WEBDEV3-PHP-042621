cc01bf432102de4ae749ad5f3fcbb51da18a3049
Angelica Quilme Manlapaz <77945056+Angelica2001143@users.noreply.github.com>
2021-04-30 19:46:18 +0800

    Edited number 3

--- number3.php
+++ number3.php
@@ -1,10 +1,10 @@
  <?php
 class Email
 {
-    public $var1;
-    public $var2;
+  
+    public $email;
 
-    public function makeForm()
+    public function Form()
     {
         echo '<form action="number3.php" method="post">
              E-mail: <input type="text" name="email" required><br>
@@ -14,20 +14,20 @@ class Email
 
     public function __construct()
     {
-        $this->var2 = isset($_POST['email']) ? $_POST['email'] : null;
+        $this->email = isset($_POST['email']) ? $_POST['email'] : null;
     }
 
     public function action()
     {
 
-        if (filter_var($this->var2, FILTER_VALIDATE_EMAIL)) {
-            echo ("$this->var2 is a valid email address");
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            echo ("$this->email - is a valid email address");
         } else {
-            echo ("$$this->var2 is not a valid email address");
+            echo ("$$this->email - is not a valid email address");
         }
     }
 }
 
-$cl = new Email();
-$cl->makeForm();
-$cl->action();
+$work = new Email();
+$work->Form();
+$work->action();
